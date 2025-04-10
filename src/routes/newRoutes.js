ff6b56216677c9a3371e6c665b648c25bc2a822b
ELyas <97180899+elyas-ely@users.noreply.github.com>
2025-04-10 13:58:54 +0430

    new one

--- src/routes/newRoutes.js
+++ src/routes/newRoutes.js
@@ -7,8 +7,23 @@ const router = express.Router()
 // ============== GET ROUTES =============
 // =======================================
 router.get('/', async (req, res) => {
-  await sendBulkPosts(1)
-  res.send('1 post created')
+  const startTime = Date.now()
+  let responseSent = false
+
+  res.write('Started creating posts.\n')
+
+  const timeoutId = setInterval(async () => {
+    if (Date.now() - startTime > 10000) {
+      clearInterval(timeoutId)
+      if (!responseSent) {
+        responseSent = true
+        res.end('Stopped after 30 seconds. Posts created.\n')
+      }
+    } else {
+      await sendBulkPosts(1)
+      console.log('Successfully sent 1/1 posts.')
+    }
+  }, 1000)
 })
 
 export default router
