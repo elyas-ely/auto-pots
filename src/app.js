42f46cb9c8141d6b5d333c31ea7cfa596bb3ccc4
ELyas <97180899+elyas-ely@users.noreply.github.com>
2025-04-10 12:27:59 +0430

    new version

--- src/app.js
+++ src/app.js
@@ -1,29 +1,27 @@
 import express from 'express'
+import newRoutes from './routes/newRoutes.js'
+
+import errorHandler from './middlewares/errorHandler.js'
 import dotenv from 'dotenv'
 import { logger } from './utils/logger.js'
-import { sendBulkPosts } from './fn.js'
 
 dotenv.config()
 
 const app = express()
-const router = express.Router()
 
 // Middleware
 app.use(express.json())
 app.use(express.static('public'))
 
 // Routes
-router.get('/', async (req, res) => {
-  res.send('<h1>Hello, world!</h1>')
-  await sendBulkPosts(1)
-  // 000
-})
+app.use('/', newRoutes)
 
-app.use('/', router)
+// Error handling
+app.use(errorHandler)
 
 const port = process.env.PORT || 3000
 
-app.listen(port, 'localhost', () => {
+app.listen(port, '0.0.0.0', () => {
   logger.info(`Server started on port ${port}`)
 })
 
---
+++ src/routes/newRoutes.js
@@ -0,0 +1,14 @@
+import { sendBulkPosts } from '../../postsAndUsers/posts.js'
+import express from 'express'
+
+const router = express.Router()
+
+// =======================================
+// ============== GET ROUTES =============
+// =======================================
+router.get('/', async (req, res) => {
+  sendBulkPosts(1)
+  res.send('1 post created')
+})
+
+export default router
