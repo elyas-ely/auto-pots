7b66926714b9fe66bd394645c33471af52d14446
ELyas <97180899+elyas-ely@users.noreply.github.com>
2025-04-10 15:03:39 +0430

    ss

--- src/routes/newRoutes.js
+++ src/routes/newRoutes.js
@@ -10,6 +10,7 @@ router.get('/', async (req, res) => {
   try {
     await sendBulkPosts(1)
     res.status(200).json({ message: 'success' })
+    console.log('success')
   } catch (error) {
     console.log(error)
   }
